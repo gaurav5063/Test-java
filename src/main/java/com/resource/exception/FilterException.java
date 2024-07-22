820766571118ae5a0809bede55264f6d3a1ffd43
gaurav5063 <mukherjeeg36@gmail.com>
2024-07-22 11:03:39 +0530

    initial commit

---
+++ src/main/java/com/resource/exception/FilterException.java
@@ -0,0 +1,12 @@
+package com.resource.exception;
+
+public class FilterException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	String message="ERROR_WHILE_FILTERING";
+
+	public FilterException(String message) {
+	    }
+
+
+}
